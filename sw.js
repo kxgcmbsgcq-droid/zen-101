7cc46c1276fdcda0b860716d62127bfdfbc0b869
Krik <verschoor@Gmail.com>
2026-03-22 18:01:22 +0100

    rebrand: 101 Zen Stories → JiGyo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='zen101-v3';
+const CACHE='jigyo-v1';
 const ASSETS=['/zen-101/','/zen-101/index.html','/zen-101/manifest.json','/zen-101/icon-192.png','/zen-101/icon-512.png','/zen-101/apple-touch-icon.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
